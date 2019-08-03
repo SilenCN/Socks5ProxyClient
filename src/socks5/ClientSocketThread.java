117b84382a8fba97ee84ab5878e2ea3177557caf
msilen <1039795376@qq.com>
2019-08-03 22:16:47 +0800

    SSL双向验证

--- src/socks5/ClientSocketThread.java
+++ src/socks5/ClientSocketThread.java
@@ -1,11 +1,18 @@
 package socks5;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.concurrent.Semaphore;
 
 
@@ -26,7 +33,7 @@ public class ClientSocketThread extends Thread {
 
             InputStream clientIn = clientSocket.getInputStream();
             OutputStream clientOut = clientSocket.getOutputStream();
-            targetSocket=new Socket(Socks5Server.TARGET_SERVER,Socks5Server.TARGET_SERVER_PORT);
+            targetSocket=getSSLTargetSocket(Socks5Server.TARGET_SERVER,Socks5Server.TARGET_SERVER_PORT);
             //传输数据转发
             Thread thread1 = new TransferThread(clientIn, targetSocket.getOutputStream());
             Thread thread2 = new TransferThread(targetSocket.getInputStream(), clientOut);
@@ -41,6 +48,21 @@ public class ClientSocketThread extends Thread {
         } finally {
             semaphore.release();
         }
+    }
+
+    public Socket getSSLTargetSocket(String host,int port) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, IOException, KeyManagementException, CertificateException {
+        SSLContext ctx = SSLContext.getInstance("SSL");
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        KeyStore ks = KeyStore.getInstance("JKS");
+        KeyStore tks = KeyStore.getInstance("JKS");
+        ks.load(new FileInputStream("C:\\Users\\lixinyu\\kclient.keystore"), "qianxin.com".toCharArray());
+        tks.load(new FileInputStream("C:\\Users\\lixinyu\\tclient.keystore"), "qianxin.com".toCharArray());
+        kmf.init(ks, "qianxin.com".toCharArray());
+        tmf.init(tks);
+        ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+        Socket sslSocket = (SSLSocket) ctx.getSocketFactory().createSocket(host, port);
+        return sslSocket;
     }
 
     public void closeClient(InputStream clientIn, OutputStream clientOut) throws IOException {
